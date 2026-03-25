d2f198b2a5e738345758b0ff921615d4fd832d89
JMIRE92 <x23398957@student.ncirl.ie>
2026-03-25 21:41:34 +0000

    Classes & JFrame Form
    dockassist (package)
    
    Java Classes:
    DocklandsRecord
    AccessLocation
    AccessIssue
    RecordListInterface
    RecordList
    StackInterface
    MyStack
    PQInterface
    PQElement
    MyPriorityQueue
    
    JFrame Form:
    DockAssistGUI

---
+++ DockAssist/src/dockassist/StackInterface.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dockassist;
+
+/**
+ *
+ * @author jamesmurphy
+ */
+public class StackInterface {
+    
+}
